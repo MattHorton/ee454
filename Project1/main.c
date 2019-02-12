1883058f0d89f6e06c368b4412d0797448ae5acc
Matt <mhorton@cphandheld.com>
2019-02-12 16:24:29 -0600

    added more adc register definitions

--- Project1/main.c
+++ Project1/main.c
@@ -15,6 +15,7 @@
 #define ADC_CR      (*((volatile unsigned long *) 0x50040008))
 #define ADC_CCR     (*((volatile unsigned long *) 0x50040300))
 #define	ADC_CFGR    (*((volatile unsigned long *) 0x5004000C))
+#define ADC_SQR1    (*((volatile unsigned long *) 0x50040030))
 
 #define TIM1        (*((volatile unsigned long *) 0x40012C00)) //timer 1
 
@@ -31,30 +32,32 @@ int main(){
 	for(i=0; i<1950; i++);
 	//for loop of 2000 is 20ms*
 	*/
-			
+	
+	
+	
 	//initializing adc ports
 	RCC_AHB2ENR |= (1 << 0); //Bit 0 is GPIOA clock enable bit
 													 //gpio a 0 and 1 MODE are already initialized to analog
 	//GPIOA_PUPDR &=~(1 << 1); //set bit 1 in ASCR to close analog switch
-		
-		
-		
+			
 	//adc stuff
-	RCC_AHB2ENR |= (1 << 13);//enable adc clock bit
-	ADC_CR |= (0 << 0);			//adc enable = 0
-	ADC_CR |= (0 << 29);     //disable deep pwd
-	//ADC_CR |= (0 << 28);     //disable internal voltage regulator
-	ADC_CCR|= (0 << 21);		 //set adc prescaler to 0 (clock not divided)
+	RCC_AHB2ENR |= (1 << 13); //enable adc clock bit
+	ADC_CR |= (0 << 0);			  //ADEN
+	ADC_CR |= (0 << 29);      //disable deep pwd
+	//ADC_CR |= (0 << 28);    //disable internal voltage regulator ADVREGEN
+	ADC_CCR|= (0 << 21);		  //set adc prescaler to 0 (clock not divided)
 	ADC_CCR|= (0 << 20);
 	ADC_CCR|= (0 << 19);
 	ADC_CCR|= (0 << 18);
-	ADC_CCR|= (0 << 17);		 //set to synchronous mode
+	ADC_CCR|= (0 << 17);		  //set to synchronous mode ADC_CCR_CKMODE
 	ADC_CCR|= (1 << 16);
-	ADC_CR |= (1 << 28);		//enable internal voltage regulator
-	ADC_CFGR|=(0 << 4);			//set resolution to 12 bits
+	ADC_CR |= (1 << 28);		  //enable internal voltage regulator
+	ADC_CFGR|=(0 << 4);			  //set resolution to 12 bits
 	ADC_CFGR|=(0 << 3);
-	ADC_CFGR|=(0 << 5);			//set data alignment to right
-		
+	ADC_CFGR|=(0 << 5);			  //set data alignment to right
+	
+	
+	
 	//pwm stuff
 	GPIOA_MODER |= (1 << 17);//set bit 17 to 1 16 to 0 for pwm gpio (PA8)
 	GPIOA_MODER |= (1 << 16);//alternate function mode
