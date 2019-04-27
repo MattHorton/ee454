7cb5b2cfa3ac00df0791ea2dc5b0c7a6c51451ed
MattHorton <mhorton@cphandheld.com>
2019-04-27 14:50:55 -0500

    added some initialization code for button scanning

--- Project3/main.c
+++ Project3/main.c
@@ -1,1 +1,19 @@
 #include "../../headerFiles/horton_stm32l432.h"
+
+int main() {
+	//initialize
+	//  adc
+	//  pwm
+	//  setpoint button PB0
+	//  I2C ?
+	
+	//enable gpiob in peripheral enable register
+	RCC->AHB2ENR |= (1<<1); //bit 1 is gpiob clock enable
+	
+	//set mode
+	GPIOB->MODER &= 0x1111111C; //set PB0 to input
+	
+	
+	
+	return 0;
+}
